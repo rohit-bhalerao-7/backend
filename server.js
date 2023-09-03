e1facf46ef365619a8558f02f3cba1b8849f1872
Rohit <rohit.bhalerao591@gmail.com>
2023-09-03 17:20:33 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const walletRoutes = require('./routes/wallet.route');
 const swaggerSpec = require('./swagger');
 const tradingPairRoutes = require('./routes/tradingpair.route');
 
-
-
 const app = express();
 const PORT = 3000;
-
 
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -30,39 +27,109 @@ io.on('connection', (socket) => {
     });
 });
 
-
 // Parse request bodies as JSON
 app.use(bodyParser.json());
 
-app.use('/users',userRoutes);
-app.use('/trades',tradeRoutes);
-
-app.use('/tokens',tokenRoutes);
-app.use('/wallets',walletRoutes);
-
-app.use('/orders',orderRoutes);
+// API Routes
+app.use('/users', userRoutes);
+app.use('/trades', tradeRoutes);
+app.use('/tokens', tokenRoutes);
+app.use('/wallets', walletRoutes);
+app.use('/orders', orderRoutes);
 app.use('/api/trading-pairs', tradingPairRoutes);
 
+// Swagger UI Route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Root Route
 app.get('/', (req, res) => {
     res.send('Hello, World!');
-  });
+});
 
-// Start the server
-app.listen(PORT, () => {
-    console.log('Server started on port 3000');
-  });
-
+// Connect to the database and start the server
 (async () => {
     try {
       await sequelize.sync({alter:true});
       console.log('Connection has been established successfully.');
   
-      // // Synchronize the models with the database
+      // Synchronize the models with the database
       await sequelize.sync({alter:true});
       console.log('Models synchronized successfully.');
+
+      // Start the server
+      server.listen(PORT, () => {
+          console.log('Server started on port 3000');
+      });
     } catch (error) {
-      console.error('Unable to connect to the database:', error)
+      console.error('Unable to connect to the database:', error);
     }
-  })()
+})();
+
+// const express = require('express');
+// const http = require('http'); 
+// const socketIo = require('socket.io');
+// const swaggerUi = require('swagger-ui-express');
+// const bodyParser = require('body-parser');
+// const {sequelize } = require('./config/db.config');
+// require('./models/index.js'); 
+// const userRoutes= require('./routes/user.route');
+// const orderRoutes = require('./routes/order.route');
+// const tradeRoutes = require('./routes/trade.route');
+// const tokenRoutes = require('./routes/token.route');
+// const walletRoutes = require('./routes/wallet.route');
+// const swaggerSpec = require('./swagger');
+// const tradingPairRoutes = require('./routes/tradingpair.route');
+
+
+
+// const app = express();
+// const PORT = 3000;
+
+
+// const server = http.createServer(app);
+// const io = socketIo(server);
+
+// io.on('connection', (socket) => {
+//     console.log('New client connected');
+
+//     socket.on('disconnect', () => {
+//         console.log('Client disconnected');
+//     });
+// });
+
+
+// // Parse request bodies as JSON
+// app.use(bodyParser.json());
+
+// app.use('/users',userRoutes);
+// app.use('/trades',tradeRoutes);
+
+// app.use('/tokens',tokenRoutes);
+// app.use('/wallets',walletRoutes);
+
+// app.use('/orders',orderRoutes);
+// app.use('/api/trading-pairs', tradingPairRoutes);
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+//   });
+
+// // Start the server
+// app.listen(PORT, () => {
+//     console.log('Server started on port 3000');
+//   });
+
+// (async () => {
+//     try {
+//       await sequelize.sync({alter:true});
+//       console.log('Connection has been established successfully.');
+  
+//       // // Synchronize the models with the database
+//       await sequelize.sync({alter:true});
+//       console.log('Models synchronized successfully.');
+//     } catch (error) {
+//       console.error('Unable to connect to the database:', error)
+//     }
+//   })()
